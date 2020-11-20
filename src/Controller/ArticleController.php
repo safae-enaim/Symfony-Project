430c841a0ff7c35942d842553e094d0876ce5923
Vathoz05 <val050313@gmail.com>
2020-11-20 15:28:37 +0100

    add article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Repository\UserRepository;
 use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,7 +16,7 @@ class ArticleController extends AbstractController
     /**
     * @Route("/", name="default", methods={"GET"})
     */
-    public function index(ArticleRepository $articleRepository, UserRepository $userRepository): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
 
         return $this->render('default/home.html.twig', [
@@ -24,4 +25,15 @@ class ArticleController extends AbstractController
         ]);
     }
     
+    /**
+    * @Route("/{id}", name="displayArticle", methods={"GET"})
+    */
+    public function oneArticle (Article $article): Response
+    {
+
+        return $this->render('article/article.html.twig', [
+            'article' => $article,
+
+        ]);
+    }
 }
